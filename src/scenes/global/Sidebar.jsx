3d83a9490a4f886ad699bd85880566c23ef0e680
HieuPP2220 <hieuphamdtu2220@gmail.com>
2023-03-20 16:36:09 +0700

    Build team, contacts, invoices

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -112,7 +112,7 @@ const Sidebar = () => {
                     >
                         Ed Roh
                     </Typography>
-                    <Typography variant="h5" color={colors.greenAccect[500]}>
+                    <Typography variant="h5" color={colors.greenAccent[500]}>
                         VP Fancy Admin
                     </Typography>
                 </Box>
@@ -121,105 +121,105 @@ const Sidebar = () => {
 
             <Box paddingLeft={isCollapsed ? undefined : "10%"}>
                 <Item
-                title="Dashboard"
-                to="/"
-                icon={<HomeOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
+                  title="Dashboard"
+                  to="/"
+                  icon={<HomeOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
                 />
 
                 <Typography
-                variant="h6"
-                color={colors.grey[300]}
-                sx={{ m: "15px 0 5px 20px" }}
+                  variant="h6"
+                  color={colors.grey[300]}
+                  sx={{ m: "15px 0 5px 20px" }}
                 >
-                Data
+                  Data
                 </Typography>
                 <Item
-                title="Manage Team"
-                to="/team"
-                icon={<PeopleOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
+                  title="Manage Team"
+                  to="/team"
+                  icon={<PeopleOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
                 />
                 <Item
-                title="Contacts Information"
-                to="/contacts"
-                icon={<ContactsOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
+                  title="Contacts Information"
+                  to="/contacts"
+                  icon={<ContactsOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
                 />
                 <Item
-                title="Invoices Balances"
-                to="/invoices"
-                icon={<ReceiptOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
+                  title="Invoices Balances"
+                  to="/invoices"
+                  icon={<ReceiptOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
                 />
 
                 <Typography
-                variant="h6"
-                color={colors.grey[300]}
-                sx={{ m: "15px 0 5px 20px" }}
+                  variant="h6"
+                  color={colors.grey[300]}
+                  sx={{ m: "15px 0 5px 20px" }}
                 >
-                Pages
+                  Pages
                 </Typography>
                 <Item
-                title="Profile Form"
-                to="/form"
-                icon={<PersonOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
+                  title="Profile Form"
+                  to="/form"
+                  icon={<PersonOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
                 />
                 <Item
-                title="Calendar"
-                to="/calendar"
-                icon={<CalendarTodayOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
+                  title="Calendar"
+                  to="/calendar"
+                  icon={<CalendarTodayOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
                 />
                 <Item
-                title="FAQ Page"
-                to="/faq"
-                icon={<HelpOutlineOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
+                  title="FAQ Page"
+                  to="/faq"
+                  icon={<HelpOutlineOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
                 />
 
                 <Typography
-                variant="h6"
-                color={colors.grey[300]}
-                sx={{ m: "15px 0 5px 20px" }}
+                  variant="h6"
+                  color={colors.grey[300]}
+                  sx={{ m: "15px 0 5px 20px" }}
                 >
-                Charts
+                  Charts
                 </Typography>
                 <Item
-                title="Bar Chart"
-                to="/bar"
-                icon={<BarChartOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
+                  title="Bar Chart"
+                  to="/bar"
+                  icon={<BarChartOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
                 />
                 <Item
-                title="Pie Chart"
-                to="/pie"
-                icon={<PieChartOutlineOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
+                  title="Pie Chart"
+                  to="/pie"
+                  icon={<PieChartOutlineOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
                 />
                 <Item
-                title="Line Chart"
-                to="/line"
-                icon={<TimelineOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
+                  title="Line Chart"
+                  to="/line"
+                  icon={<TimelineOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
                 />
                 <Item
-                title="Geography Chart"
-                to="/geography"
-                icon={<MapOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
+                  title="Geography Chart"
+                  to="/geography"
+                  icon={<MapOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
                 />
             </Box>
         </Menu>
--- src/theme.js
+++ src/theme.js
@@ -27,7 +27,7 @@ export const tokens = (mode) => ({
                 800: "#080b12",
                 900: "#040509"
             },
-            greenAccect: {
+            greenAccent: {
                 100: "#dbf5ee",
                 200: "#b7ebde",
                 300: "#94e2cd",
@@ -38,7 +38,7 @@ export const tokens = (mode) => ({
                 800: "#1e5245",
                 900: "#0f2922"
             },
-            redAccect: {
+            redAccent: {
                 100: "#f8dcdb",
                 200: "#f1b9b7",
                 300: "#e99592",
@@ -49,7 +49,7 @@ export const tokens = (mode) => ({
                 800: "#58201e",
                 900: "#2c100f"
             },
-            blueAccect: {
+            blueAccent: {
                 100: "#e1e2fe",
                 200: "#c3c6fd",
                 300: "#a4a9fc",
@@ -83,7 +83,7 @@ export const tokens = (mode) => ({
                 800: "#a1a4ab",
                 900: "#d0d1d5",
             },
-            greenAccect: {
+            greenAccent: {
                 100: "#0f2922",
                 200: "#1e5245",
                 300: "#2e7c67",
@@ -94,7 +94,7 @@ export const tokens = (mode) => ({
                 800: "#b7ebde",
                 900: "#dbf5ee",
             },
-            redAccect: {
+            redAccent: {
                 100: "#2c100f",
                 200: "#58201e",
                 300: "#832f2c",
@@ -105,7 +105,7 @@ export const tokens = (mode) => ({
                 800: "#f1b9b7",
                 900: "#f8dcdb",
             },
-            blueAccect: {
+            blueAccent: {
                 100: "#151632",
                 200: "#2a2d64",
                 300: "#3e4396",
@@ -132,7 +132,7 @@ export const themeSettings = (mode) => {
                         main: colors.primary[500],
                     },
                     secondary: {
-                        main: colors.greenAccect[500],
+                        main: colors.greenAccent[500],
                     },
                     neutral: {
                         dark: colors.grey[700],
@@ -147,7 +147,7 @@ export const themeSettings = (mode) => {
                         main: colors.primary[100],
                     },
                     secondary: {
-                        main: colors.greenAccect[500],
+                        main: colors.greenAccent[500],
                     },
                     neutral: {
                         dark: colors.grey[700],
